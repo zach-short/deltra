bf68da5fde6b8a5225c97ee8c249c760a0ab548b
zachmshort <zach.short@fantomworks.com>
2025-07-02 11:42:17 -0400

    udpate to constants (env vars)

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -3,7 +3,7 @@ export const REFRESH_COOKIE_NAME = 'refresh_token';
 export const COOKIE_MAX_AGE = 20; // 20 seconds
 export const JWT_EXPIRATION_TIME = '20s'; // 20 seconds
 export const REFRESH_TOKEN_EXPIRY = '30d'; // 30 days
-export const REFRESH_TOKEN_MAX_AGE = 30 * 24 * 60 * 60; // 30 days in seconds
+export const REFRESH_TOKEN_MAX_AGE = 30 * 24 * 60 * 60; // 30 days
 
 export const REFRESH_BEFORE_EXPIRY_SEC = 60;
 
@@ -12,7 +12,7 @@ export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 export const GOOGLE_REDIRECT_URI = `${process.env.EXPO_PUBLIC_BASE_URL}/api/auth/callback`;
 export const GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
 
-export const APPLE_CLIENT_ID = 'com.beto.expoauthexample.web';
+export const APPLE_CLIENT_ID = 'com.deltra.app.web';
 export const APPLE_CLIENT_SECRET = process.env.APPLE_CLIENT_SECRET!;
 export const APPLE_REDIRECT_URI = `${process.env.EXPO_PUBLIC_BASE_URL}/api/auth/apple/callback`;
 export const APPLE_AUTH_URL = 'https://appleid.apple.com/auth/authorize';
