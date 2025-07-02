6fe3c61b93b4f4ecb5b2239e99134c0aa2f65f06
zachmshort <zach.short@fantomworks.com>
2025-07-02 11:41:57 -0400

    error with oath not posting correctly

--- backend/models/userModel.go
+++ backend/models/userModel.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type User struct {
-	ID         string `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-	Name       string
-	Email      string `gorm:"unique"`
-	Provider   string
-	ProviderID string `gorm:"unique"`
-	Picture    string
-	CreatedAt  time.Time `gorm:"autoCreateTime"`
-	UpdatedAt  time.Time `gorm:"autoUpdateTime"`
+	ID         string `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Name       string `json:"name"`
+	Email      string `gorm:"unique" json:"email"`
+	Provider   string `gorm:"uniqueIndex:idx_provider_user" json:"provider"`
+	ProviderID string `gorm:"uniqueIndex:idx_provider_user" json:"provider_id"`
+	Picture    string `json:"picture"`
+	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
